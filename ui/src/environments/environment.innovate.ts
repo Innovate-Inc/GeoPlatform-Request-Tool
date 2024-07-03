6dadc55f978423ab3a0564d154328d38e73785a1
Travis <tbock@innovateteam.com>
2024-07-03 14:35:23 -0700

    add innovate env for frontend

---
+++ ui/src/environments/environment.innovate.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  oauth_url: 'https://request.itgis.innovateteam.com/api/oauth2',
+  local_service_endpoint: 'https://request.itgis.innovateteam.com/api',
+  api_url: 'https://request.itgis.innovateteam.com/api',
+  recaptcha_siteKey: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
+  snackbar_duration: 6000
+};
