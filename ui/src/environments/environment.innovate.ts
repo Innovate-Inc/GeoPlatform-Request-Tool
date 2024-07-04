e338d8aca5aa8156266574fd31580452ea575b5c
Travis <tbock@innovateteam.com>
2024-07-03 18:16:07 -0700

    env was bad and I hate everything

--- ui/src/environments/environment.innovate.ts
+++ ui/src/environments/environment.innovate.ts
@@ -1,8 +1,7 @@
 export const environment = {
   production: true,
   oauth_url: 'https://request.itgis.innovateteam.com/api/oauth2',
-  local_service_endpoint: 'https://request.itgis.innovateteam.com/api',
-  api_url: 'https://request.itgis.innovateteam.com/api',
-  recaptcha_siteKey: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
+  local_service_endpoint: '/api',
+  recaptcha_siteKey: '6Ld14wcqAAAAAKrUfTlYqyUyuLzkNUSQiiPTxM2N',
   snackbar_duration: 6000
 };
